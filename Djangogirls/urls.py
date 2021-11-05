f1e524f12fa3a9b0d2910edf1baa7a9b85a75d84
Bektur <macbook@macbooks-MacBook-Pro.local>
2021-11-05 13:53:45 +0600

    Commit

--- Djangogirls/urls.py
+++ Djangogirls/urls.py
@@ -1,5 +1,4 @@
-"""Djangogirls URL Configuration
-
+"""mysite URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -19,4 +18,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+
 ]
